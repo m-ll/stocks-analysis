1357254d273f87ccf10694d1a92b3506de566c9c
m-ll <43573326+m-ll@users.noreply.github.com>
2018-12-07 11:28:41 +0100

    chore: add copyright header in each source file

--- renderer/data.py
+++ renderer/data.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+#
+# Copyright (c) 2018-19 m-ll. All Rights Reserved.
+#
+# Licensed under the MIT License.
+# See LICENSE file in the project root for full license information.
+#
+# 2b13c8312f53d4b9202b6c8c0f0e790d10044f9a00d8bab3edf3cd287457c979
+# 29c355784a3921aa290371da87bce9c1617b8584ca6ac6fb17fb37ba4a07d191
+#
 
 import copy
 import math
